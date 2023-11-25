bcc371092e867575d4187cb5c57a8abbd06089a2
alik-r <alikamran.rza@gmail.com>
2023-11-25 18:38:58 +0400

    Implement Euler's Phi function

---
+++ algorithms/euler_phi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int phi(int n) {
+    int result = n; 
+
+    for (int p = 2; p * p <= n; ++p) {
+        if (n % p == 0) {
+            while (n % p == 0) {
+                n /= p;
+            }
+            result -= result / p;
+        }
+    }
+
+    if (n > 1) {
+        result -= result / n;
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "The value of Euler's Phi function for " << n << " is " << phi(n) << '\n';
+}
+
