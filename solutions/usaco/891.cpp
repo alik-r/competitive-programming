1db7d80f70d703a8a558f9f66f4f5a5b43dc9aff
Alikamran Rzayev <alikamran.rza@gmail.com>
2025-02-22 13:57:50 +0400

    Improve usaco/891.cpp

--- solutions/usaco/891.cpp
+++ solutions/usaco/891.cpp
@@ -19,22 +19,16 @@ int main() {
     int n;
     cin >> n;
     
-    int first = 0;
-    int second = 0;
-    int third = 0;
     vector<int> v = {1, 2, 3};
+    vector<int> wins(3, 0);
     for (int i = 0; i < n; i++) {
         int a, b, g;
         cin >> a >> b >> g;
-        a--;
-        b--;
-        g--;
+        a--, b--, g--;
         swap(v[a], v[b]);
-        first += int(v[g] == 1);
-        second += int(v[g] == 2);
-        third += int(v[g] == 4);
+        wins[g]++;
     }
 
-    int res = max(first, max(second, third));
+    int res = max(wins[0], max(wins[1], wins[2]));
     cout << res << '\n';
 }
